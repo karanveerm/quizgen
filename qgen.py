03bd4213dec35e40d35de3b44b1d56288b99f380
Karanveer Mohan <karanveer.1992@gmail.com>
2014-05-02 07:09:14 -0700

    pack qgen into one file

--- qgen.py
+++ qgen.py
@@ -1,8 +1,202 @@
+#!/usr/bin/env python
+
+# The MIT License (MIT)
+
+# Copyright (c) 2014 Karanveer Mohan
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from xml.dom import minidom
-import quiz_parser
 import sys
 import re
 import glob
+import itertools
+
+
+"""
+Parse the Quiz to create a python dict
+"""
+
+class QuizParser():
+  """Parses the quiz and returns it in a python dict"""
+  def __init__(self, filename):
+    if '.quiz' in filename:
+      self.filename = filename
+    else:
+      self.filename = '%s.quiz' % filename
+
+
+  def get_filename(self):
+    return self.filename
+
+
+  def _make_backwards_compatible(self, lines):
+    """
+    Takes care of case where there is an empty line between question and options
+    that existed in EE364A.
+    """
+    i = 0
+    while True:
+      if i == len(lines) - 1: break
+      if not lines[i] and lines[i + 1].startswith('*'):
+        del lines[i]
+      i += 1
+    return lines
+
+
+  def _jump_to_non_blank_line(self, lines):
+    """
+    Returns the list of lines starting from the first line that is not empty.
+    Returns [] if no lines remain
+    """
+    while lines and not lines[0]:
+      lines = lines[1:]
+    return lines
+
+
+  def _parse_title(self, lines):
+    """
+    The quiz must begin with a "== " followed by an optional title.
+    If the title exists, it returns th title, empty string otherwise
+    """
+    first_line = lines[0]
+    if first_line.startswith('== '):
+      return first_line[3:]
+    raise Exception('Invalid format. Quiz must start with "== " followed by an optional title.')
+
+
+  def _parse_new_problem_group(self, line_group):
+    """
+    Parses a new problem group, i.e. the title of the main problem and the statement.
+    Each problem group can have multiple questions associated with it.
+    """
+    title_line = line_group[0]
+    if title_line.rfind(']') == -1:
+      raise Exception('Problem title must be in the form [TITLE] with the square brackets')
+    problem_intro = line_group[1:]
+
+    return {
+      # [Sensitivity Analysis] ==> 'Sensitivity Analysis'
+      'problem_title': title_line[1:title_line.rfind(']')],
+      'problem_intro': '\n'.join(problem_intro),
+      'questions': []
+    }
+
+
+  def _parse_explanation_and_description(self, line):
+    """
+    Find the description for an option, and an explanation (if any)
+    """
+    if line.find('::') != -1:
+      # We have an explanation for this option
+      explanation = line[line.find('::') + 2:]
+      description = line[line.find(' ') + 1:line.find('::')]
+      has_explanation = True
+    else:
+      explanation = ''
+      description = line[line.find(' ') + 1:]
+      has_explanation = False
+
+    return has_explanation, explanation, description
+
+
+  def _parse_new_question(self, line_group):
+    """
+    Parses a new question for a given problem group. A question consists of a
+    description and a number of options. A correct option begins with *= and an
+    incorrect option begins with *. An explanation begins with ::
+    """
+    question = {
+      'description': '',
+      'options': []
+    }
+
+    while not line_group[0].startswith('*'):
+      question['description'] += '\n' + line_group[0]
+      line_group = line_group[1:]
+
+    for line in line_group:
+      if line[0] == '*':
+        # New option
+        has_explanation, explanation, description = self._parse_explanation_and_description(line)
+        option = {
+          'explanation': explanation,
+          'description': description,
+          'correct': True if line.startswith('*= ') else False
+        }
+        question['options'].append(option)
+      else:
+        # Continuation of the description or explanation of the previous option
+        if has_explanation:
+          question['options'][-1]['explanation'] += '\n' + line
+        else:
+          has_explanation, explanation, description = self._parse_explanation_and_description(line)
+          question['options'][-1]['description'] += '\n' + description
+          question['options'][-1]['explanation'] += '\n' + explanation
+
+    return question
+
+
+  def parse(self):
+    # Open the file and read in the lines
+    try:
+      quizfile = open(self.filename, 'r')
+    except IOError:
+      raise Exception('No file named %s found' % self.filename)
+
+    lines = quizfile.readlines()
+    lines = [line.strip() for line in lines]
+    lines = self._make_backwards_compatible(lines)
+
+    quiz = {
+      'title': self._parse_title(lines),
+      'problem_groups': []
+    }
+    lines = self._jump_to_non_blank_line(lines[1:])
+
+    for is_empty_line, line_group in itertools.groupby(lines, lambda line: not line):
+      if is_empty_line: continue
+
+      line_group = list(line_group)
+
+      if line_group[0].startswith('[') and any(line.startswith('*') for line in line_group):
+        # Grotesque code needed for backwards compatibility
+        # Problem groups need not have intros so a [TITLE] can be immediately followed by
+        # a question. This takes care of that case
+        problem_group = self._parse_new_problem_group(line_group)
+        problem_group['problem_intro'] = ''
+        quiz['problem_groups'].append(problem_group)
+        line_group = line_group[1:]
+
+      if line_group[0].startswith('['):
+        # Marks the beginning of a new problem group
+        problem_group = self._parse_new_problem_group(line_group)
+        quiz['problem_groups'].append(problem_group)
+      else:
+        # This is a single question that corresponds to the last problem_group in the line_group
+        question = self._parse_new_question(line_group)
+        try:
+          quiz['problem_groups'][-1]['questions'].append(question)
+        except:
+          raise Exception('ERROR. Are you sure you started every problem group with "[]"?')
+
+    return quiz
 
 
 def create_single_choice_dom_from_option(option):
@@ -191,9 +385,12 @@ def add_dom_to_template(dom, html_file_name, quiz):
   will be added and a [TITLE] holder for title.
   """
   generated_file = open(html_file_name, 'w+')
-  template_file =  open('template.html')
+  try:
+    template_file = open('template.html')
+    content = template_file.read()
+  except IOError:
+    content = HTML
 
-  content = template_file.read()
   content = content.replace('[TITLE]', quiz['title'])
   content = content.replace('[BODY]', dom.toprettyxml())
 
@@ -206,18 +403,230 @@ def add_dom_to_template(dom, html_file_name, quiz):
   generated_file.write(content)
   generated_file.close()
 
+  # Create the CSS file if it does not exist
+  try:
+    css_file = open('quiz.css')
+  except IOError:
+    print 'No CSS file called quiz.css found in directory. Using default CSS'
+    generated_css_file = open('quiz.css', 'w+')
+    generated_css_file.write(CSS)
+    generated_css_file.close()
+
+
+def usage():
+  print """
+  Usage: python qgen.py SOURCE_QUIZ_FILE
+  You may like to sudo cp qgen.py /usr/bin/qgen
+  Remember to sudo chmod +x /usr/bin/qgen to make the file executable
+
+  You can use qgen simply by typing
+  qgen index
+  which will product an index.html from index.quiz file.
+  In case no CSS styling is provided, it will also create a quiz.css file.
+
+  More information and a sample quiz file can be found on
+  https://github.com/karanveerm/QGen
+  """
+
+
+# HTML template
+# TODO: Don't judge, I didn't want to do this
+HTML = r"""
+<!DOCTYPE html">
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>[TITLE]</title>
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Sans|Alike' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="quiz.css" type="text/css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/x-mathjax-config">
+      MathJax.Hub.Config({
+        tex2jax: {
+          inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+          processEscapes: true
+        }
+      });
+    </script>
+    <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML.js"></script>
+
+    <script type="text/javascript">
+    $(document).ready(function(){
+      //close all the content divs on page load
+      $('.response').hide();
+
+      // toggle slide
+      $('.selection').click(function(){
+        // by calling sibling, we can use same div for all demos
+        $(this).siblings('.response').slideToggle('fast');
+      });
+
+      $('button').click(function(event){
+        var $target = $(event.target);
+        $target.parent('.mcq').find('.response').slideToggle('fast');
+        if ($target.text() == 'Submit') {
+          $target.text('Hide');
+        } else {
+          $target.text('Submit');
+        }
+      });
+    });
+    </script>
+  </head>
+
+<body>
+  $\newcommand{\ones}{\mathbf 1}$
+  [BODY]
+  <footer>
+  Page generated using <a href="https://github.com/karanveerm/QGen">QGen</a>
+  </footer>
+</body>
+</html>
+"""
+
+# CSS template
+# TODO: This is not something I'm proud of
+CSS = """
+html {
+  min-height: 100%;
+}
+
+body {
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0;
+  font-family: 'PT Sans', 'Helvetica Neue', Helvetica, Arial, Sans-serif;
+  font-size: 16px;
+  position: relative;
+  min-height: 100%;
+  width: 680px;
+  background-color: rgb(250, 250, 250);
+  color: #333;
+}
+
+img {
+  height: 400px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px;
+}
+
+div .description {
+  margin-top: 30px;
+}
+
+a {
+  color: #000066;
+}
+
+div .selection {
+  color: #000066;
+  text-decoration: none;
+}
+
+div .selection:hover {
+  text-decoration: underline;
+}
+
+
+span.right {
+  color:#008800;
+}
+
+hr {
+  width: 65%;
+  margin-left: 0px;
+  border: 0px;
+  border-bottom: 1px solid #ccc;
+}
+
+fieldset {
+  border: 0px;
+  border-bottom: 2.5px solid #ccc;
+  padding: 10px 20px;
+  padding-left: 0px;
+  margin-bottom: 0px;
+  margin-top: 0px;
+}
+
+ol ul {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+.selection {
+  cursor: pointer;
+  margin-top: 5px;
+}
+
+.multiple-selection {
+  cursor: pointer;
+  margin-top: 5px;
+}
+
+legend {
+  font-family: Alike, 'Josefin Sans';
+  font-size: 19px;
+  padding-left: 0px;
+}
+
+
+.response.wrong{
+  padding: 8px 10px;
+  color: #D8000C;
+  background-color: #FFBABA;
+}
+
+.response.right{
+  padding: 8px 10px;
+  color: #4F8A10;
+  background-color: #DFF2BF;
+}
+
+footer {
+  font-size: 12px;
+  margin-top: 0px;
+  margin-bottom: 5px;
+  bottom: 0;
+  width: 100%;
+}
+
+label {
+  display: block;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+button {
+  background: #bbb;
+  border: 0px;
+  color: #ffffff;
+  font-size: 14px;
+  padding: 6px 12px 6px 12px;
+  text-decoration: none;
+}
+
+button:hover{
+  background: #ccc;
+}
+
+button:focus {
+    outline: none;
+}
+"""
+
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
-    raise Exception('Usage: python generate_quiz.py quiz_name.quiz generated_html_name.html')
+    usage()
+  else:
+    for filename in sys.argv[1:]:
+      quiz_parser = QuizParser(filename)
 
-  for filename in sys.argv[1:]:
-    quiz = quiz_parser.parse(filename)
+      quiz = quiz_parser.parse()
 
-    dom = create_dom_from_quiz(quiz)
+      dom = create_dom_from_quiz(quiz)
+      html_file_name = quiz_parser.get_filename().replace('.quiz', '.html')
 
-    if len(sys.argv) > 2 and '.html' in sys.argv[2]:
-      html_file_name = sys.argv[2]
-    else:
-      html_file_name = filename.replace('.quiz', '.html')
-    add_dom_to_template(dom, html_file_name, quiz)
+      add_dom_to_template(dom, html_file_name, quiz)
